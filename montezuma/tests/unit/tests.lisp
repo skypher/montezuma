d00fa51cf055649dad3533e0806e3a9991a482ae
John Wiseman <jjwiseman@gmail.com>
2005-12-22 09:01:21 +0000

    Made tests less verbose.

--- montezuma/tests/unit/tests.lisp
+++ montezuma/tests/unit/tests.lisp
@@ -44,15 +44,18 @@
   (assert (not (assoc name *failed-tests*)) nil "There is already a test named ~S." name)
   (assert (not (assoc name *passed-tests*)) nil "There is already a test named ~S." name)
   (push (cons name (list expr value expected-value)) *failed-tests*)
-  (warn "FAILURE: Test ~S: ~S evaluated to ~S instead of ~S."
-	name expr value expected-value)
+;;  (warn "FAILURE: Test ~S: ~S evaluated to ~S instead of ~S."
+;;	name expr value expected-value)
+  (format T "F")
   nil)
 
 (defun test-success (name expr value expected-value)
   (assert (not (assoc name *failed-tests*)) nil "There is already a test named ~S." name)
   (assert (not (assoc name *passed-tests*)) nil "There is already a test named ~S." name)
   (push (cons name (list expr value expected-value)) *passed-tests*)
-  (format T "~&Test ~S passed.~%" name))
+;;  (format T "~&Test ~S passed.~%" name)
+  (format T ".")
+)
 
 (defun begin-tests ()
   (setf *passed-tests* '())
