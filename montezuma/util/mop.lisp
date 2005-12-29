fac55bc4f8e18ed1eabd8e9001bab36546b5186d
John Wiseman <jjwiseman@gmail.com>
2005-12-29 07:31:48 +0000

    Added sbcl support.

--- montezuma/util/mop.lisp
+++ montezuma/util/mop.lisp
@@ -1,7 +1,11 @@
 (in-package #:montezuma)
 
 (defun class-slots (class)
-  (ccl:class-slots class))
+  #+openmcl (ccl:class-slots class)
+  #+sbcl (sb-mop:class-slots class))
+  
 
 (defun slot-definition-name (slot-defn)
-  (ccl:slot-definition-name slot-defn))
+  #+openmcl (ccl:slot-definition-name slot-defn)
+  #+sbcl (sb-mop:slot-definition-name slot-defn))
+
