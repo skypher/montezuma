86851104410c05dbb5dcd24a6a883ff3e50aebce
John Wiseman <jjwiseman@gmail.com>
2006-01-03 19:40:06 +0000

    Added dosequence macro.

--- montezuma/util/while.lisp
+++ montezuma/util/while.lisp
@@ -5,3 +5,22 @@
        ((not ,expr))
      ,@body))
 
+#||
+(defmacro dosequence ((var sequence &optional ret) &body body)
+  `(let ((,var nil))
+     (map nil
+	  #'(lambda (,var)
+	      ,@body body)
+	  ,sequence)
+     ,ret))
+||#
+
+(defmacro dosequence ((var sequence &key result index) &body body)
+  `(let ((,var nil)
+	 ,@(if index `((,index 0)) nil))
+     (map nil
+	  #'(lambda (,var)
+	      ,@body
+	      ,@(if index `((incf ,index)) nil))
+	  ,sequence)
+     ,result))
