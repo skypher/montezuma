0c8132c1779d6fae8a11ca58d5f56dd15770b504
John Wiseman <jjwiseman@gmail.com>
2006-01-20 00:46:42 +0000

    Added dosequence and do-range.

--- montezuma/util/while.lisp
+++ montezuma/util/while.lisp
@@ -16,11 +16,28 @@
 ||#
 
 (defmacro dosequence ((var sequence &key result index) &body body)
-  `(let ((,var nil)
-	 ,@(if index `((,index 0)) nil))
-     (map nil
-	  #'(lambda (,var)
-	      ,@body
-	      ,@(if index `((incf ,index)) nil))
-	  ,sequence)
-     ,result))
+  `(block nil
+     (let ((,var nil)
+	   ,@(if index `((,index 0)) nil))
+       (map nil
+	    #'(lambda (,var)
+		,@body
+		,@(if index `((incf ,index)) nil))
+	    ,sequence)
+       ,result)))
+
+;; Hey, it's just like Python's range function!
+(defmacro do-range ((var start-form end-form &optional result-form) &body body)
+  "Iterates VAR through the range of integers in [START-FORM,
+  END-FORM).  Returns the value of END-FORM (at the time END-FORM is
+  evaluated, VAR is bound to the value of END-FORM.
+
+  (do-range (i 10 (length s))
+    (print (elt s i)))"
+  (let ((start-var (gensym))
+	(end-var (gensym)))
+    `(let ((,start-var ,start-form)
+	   (,end-var ,end-form))
+       (do ((,var ,start-var (1+ ,var)))
+	   ((>= ,var ,end-var) ,result-form)
+	 ,@body))))
