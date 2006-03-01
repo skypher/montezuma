ba814618fbda02f900d9b7324a3a9ba42c36f13e
John Wiseman <jjwiseman@gmail.com>
2006-03-01 22:02:56 +0000

    Moved the cloning protocol to mop.lisp.

--- montezuma/store/buffered-index-io.lisp
+++ montezuma/store/buffered-index-io.lisp
@@ -73,23 +73,6 @@
     (setf buffer (make-array (list buffer-size)))))
 
 
-(defun clone (object)
-  (let ((clone (clone-object object)))
-    (initialize-copy clone object)
-    clone))
-
-(defmethod clone-object ((object T))
-  (let ((copy (allocate-instance (class-of object))))
-    (loop for slot in (class-slots (class-of object))
-       when (slot-boundp object (slot-definition-name slot))
-       do (setf (slot-value copy (slot-definition-name slot))
-		(slot-value object (slot-definition-name slot))))
-    copy))
-
-
-(defmethod initialize-copy (self o)
-  (declare (ignore self) (ignore o)))
-
 (defmethod initialize-copy :after ((self buffered-index-input) o)
   (with-slots (buffer buffer-size buffer-start buffer-length buffer-position) self
     (let ((b (slot-value o 'buffer)))
--- montezuma/util/mop.lisp
+++ montezuma/util/mop.lisp
@@ -12,3 +12,23 @@
   #+openmcl (ccl:slot-definition-name slot-defn)
   #+sbcl (sb-mop:slot-definition-name slot-defn))
 
+
+;; Something approximating the Ruby clone protocol.
+
+(defun clone (object)
+  (let ((clone (clone-object object)))
+    (initialize-copy clone object)
+    clone))
+
+(defmethod clone-object ((object T))
+  (let ((copy (allocate-instance (class-of object))))
+    (loop for slot in (class-slots (class-of object))
+       when (slot-boundp object (slot-definition-name slot))
+       do (setf (slot-value copy (slot-definition-name slot))
+		(slot-value object (slot-definition-name slot))))
+    copy))
+
+
+(defmethod initialize-copy (self o)
+  (declare (ignore self) (ignore o)))
+
