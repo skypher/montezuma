1783a06fa8585189714df5f80f4a91f93344969a
Gary King <gwking@metabang.com>
2006-04-04 17:20:54 +0000

    Added utils/comparable.lisp

---
+++ montezuma/util/comparable.lisp
@@ -0,0 +1,9 @@
+(in-package montezuma)
+
+(defgeneric <=> (a b)
+  (:documentation "Ruby three way comparison operator. Returns -1, 0, or 1."))
+
+(defmethod <=> ((a number) (b number))
+  (cond ((= a b) 0)
+        ((< a b) -1)
+        (t 1)))
