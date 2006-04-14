21c09f031490e1435d50f4861a18a9edd58febf3
Gary King <gwking@metabang.com>
2006-04-14 03:33:57 +0000

    Added utilities.lisp (with the ever exciting length-1-list-p)

---
+++ montezuma/util/utilities.lisp
@@ -0,0 +1,6 @@
+(in-package montezuma)
+
+(declaim (inline length-1-list-p))
+(defun length-1-list-p (x) 
+  "Is x a list of length 1? Note that this is better than the naive \(= \(length x\) 1\) because we don't need to traverse the entire list..."
+  (and (consp x) (null (cdr x))))
