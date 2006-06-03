558f3b57f3078fa8fce49771bee733ea3862fbe1
John Wiseman <jjwiseman@gmail.com>
2006-06-03 20:53:40 +0000

    Oops, gotta make the build directory.

--- montezuma/tests/corpora/pastes-1000/lucene-indexer/build.sh
+++ montezuma/tests/corpora/pastes-1000/lucene-indexer/build.sh
@@ -5,4 +5,6 @@ BUILD=${BASE_DIR}/build
 LIB=${BASE_DIR}/lib
 SRC=${BASE_DIR}/src
 
+mkdir ${BUILD} 2>/dev/null
+
 javac -d ${BUILD} -classpath ${LIB}/json_simple.jar:${LIB}/lucene-1.4.3.jar ${SRC}/PasteIndexer.java
