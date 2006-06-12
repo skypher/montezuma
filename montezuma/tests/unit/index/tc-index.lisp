afee2429d09d579450ce2ff8e0986d3e1dd3d7d5
John Wiseman <jjwiseman@gmail.com>
2006-06-12 05:40:05 +0000

    0 of 2349 tests fail.

--- montezuma/tests/unit/index/tc-index.lisp
+++ montezuma/tests/unit/index/tc-index.lisp
@@ -15,7 +15,9 @@
     (atest search-results-correct
 	   (reverse results)
 	   expected
-	   #'set=)))
+	   #'set=
+	   (format T "~&Expected ~S, got ~S, query was ~S." expected (reverse results) query))))
+    
 
 (defun do-test-index-with-array (index)
   (let ((data '(#("one two")
@@ -40,9 +42,11 @@
 			    :term (make-term (slot-value index 'default-search-field)
 					     term))))
       (check-query-results index (term-query "one") '(0 1 3 4 6))
+      (check-query-results index "one" '(0 1 3 4 6))
       (let ((query (make-instance 'boolean-query)))
 	(add-query query (term-query "one") :must-occur)
 	(check-query-results index query '(0 1 3 4 6)))
+      (check-query-results index "+one" '(0 1 3 4 6))
       (let ((query (make-instance 'boolean-query)))
 	(add-query query (term-query "one") :should-occur)
 	(check-query-results index query '(0 1 3 4 6)))
@@ -50,15 +54,18 @@
 	(add-query query (term-query "one") :must-occur)
 	(add-query query (term-query "two") :must-occur)
 	(check-query-results index query '(0 4)))
+      (check-query-results index "+one +two" '(0 4))
       (let ((query (make-instance 'boolean-query)))
 	(add-query query (term-query "two") :must-occur)
 	(add-query query (term-query "three") :must-occur)
 	(add-query query (term-query "four") :must-occur)
 	(check-query-results index query '(5 7)))
+      (check-query-results index "+two +three +four" '(5 7))
       (let ((query (make-instance 'boolean-query)))
 	(add-query query (term-query "one") :should-occur)
 	(add-query query (term-query "five") :should-occur)
-	(check-query-results index query '(0 1 3 4 6 7))))
+	(check-query-results index query '(0 1 3 4 6 7)))
+      (check-query-results index "one five" '(0 1 3 4 6 7)))
     (atest index-with-array-2
 	   (document-values (get-document index 7) "def_field")
 	   "two three four five"
