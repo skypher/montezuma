df548c9ba6c863ccc6c05afc0aba558b9f5d83a4
John Wiseman <jjwiseman@gmail.com>
2006-06-21 21:09:05 +0000

    Fixing the Planet Lisp web search example a bit.

--- montezuma/tests/corpora/planet-lisp/web-search.lisp
+++ montezuma/tests/corpora/planet-lisp/web-search.lisp
@@ -1,12 +1,9 @@
 (eval-when (:compile-toplevel :load-toplevel :execute)
-  (cl:require :tbnl)
-  (cl:require :cl-who)
-  (cl:require :html-template))
+  (require :tbnl)
+  (require :cl-who)
+  (require :html-template))
 
-(defpackage :pl-search
-  (:use :tbnl :common-lisp :html-template))
-
-(in-package :pl-search)
+(in-package #:planet-lisp-search)
 
 (setq tbnl:*show-lisp-errors-p* T)
 (setq tbnl:*show-lisp-backtraces-p* T)
@@ -14,7 +11,7 @@
 
 (setq tbnl:*dispatch-table*
       (list
-       (tbnl:create-prefix-dispatcher "/index.html" 'main-page)
+       (tbnl:create-prefix-dispatcher "/" 'main-page)
        (tbnl:create-prefix-dispatcher "/cache.html" 'cache-page)))
        
 
@@ -24,32 +21,25 @@
 					   :defaults *load-pathname*))
 
 (defparameter *main-query-parser* (make-instance 'montezuma::query-parser
-						 :default-field "contents"))
-(defparameter *test-query-parser* (make-instance 'montezuma::test-query-parser
-						 :default-field "contents"))
+						 :fields '("title" "description" "text")))
+
 
 (defun parse-query (q)
   (montezuma::parse *main-query-parser* q))
-(defun query-parse-tree (q)
-  (montezuma::parse *test-query-parser* q))
+
 
 (defun get-field (doc name)
   (montezuma::field-data (montezuma::document-field doc name)))
 
-(defvar *pl-index*
-  (make-instance 'montezuma::index
-		 :path (make-pathname :name "plindex"
-				      :defaults montezuma::*pl-directory*)
-		 :create-p NIL
-		 :default-field "contents"))
-
 
 (defun cache-page ()
+  (unless *index*
+    (load-index))
   (let ((cache-tmpl (merge-pathnames (make-pathname :name "cache"
 						    :type "tmpl")
 				     *pl-web-root*)))
     (let ((id (tbnl:get-parameter "id")))
-      (let ((doc (montezuma::get-document *pl-index* id)))
+      (let ((doc (montezuma::get-document *index* id)))
 	(if doc
 	    (with-output-to-string (s)
 	      (html-template:fill-and-print-template
@@ -59,43 +49,35 @@
 	       :stream s))
 	    (error "No document with id ~S seems to exist." id))))))
 
+
 (defun main-page ()
+  (unless *index*
+    (load-index))
   (let ((main-tmpl (merge-pathnames (make-pathname :name "index"
 						   :type "tmpl")
 				    *pl-web-root*)))
     (let* ((query-string (tbnl:get-parameter "q"))
+	   (start-doc (parse-integer (or (tbnl:get-parameter "start") "0")))
 	   (escaped-query-string (cl-who:escape-string query-string))
-	   (query-parse-tree nil)
 	   (query-object nil)
 	   (results nil)
 	   (num-results nil)
 	   (query-time nil))
       (when query-string
 	(setf query-string (string-trim '(#\space #\tab) query-string))
-	(setf query-parse-tree
-	      (let ((tree (query-parse-tree query-string)))
-		(if tree
-		    (format nil "~%<pre>~A</pre>~%"
-			    (cl-who:escape-string
-			     (with-output-to-string (s)
-			       (pprint tree s))))
-		    (format nil "<p class=\"error\">Your query, &ldquo;~A&rdquo;, could not be parsed.  Sorry!</p>"
-			    escaped-query-string))))
 	(setf query-object
 	      (let ((query (parse-query query-string)))
-		(if (not query)
-		    nil
-		    (format nil "~%<pre>~A</pre>~%"
-			    (cl-who:escape-string
-			     (with-output-to-string (s)
-			       (pprint query s)))))))
+		(format nil "~%<pre>~A</pre>~%"
+			(cl-who:escape-string
+			 (with-output-to-string (s)
+			   (pprint query s))))))
 	(let ((query (parse-query query-string)))
 	  (when query
 	    (let ((start-time (get-internal-real-time)))
 	      (montezuma::search-each
-	       *pl-index* query
+	       *index* query
 	       #'(lambda (doc score)
-		   (let ((document (montezuma::get-document *pl-index* doc)))
+		   (let ((document (montezuma::get-document *index* doc)))
 		     (push (list :score (format nil "~,3F" score)
 				 :date (get-field document "date")
 				 :title (let ((title (get-field document "title")))
@@ -105,21 +87,21 @@
 				 :id (tbnl:url-encode (get-field document "id"))
 				 :link (cl-who:escape-string (get-field document "link")))
 			   results)))
-	       '(:num-docs 50))
+	       (list :num-docs 10 :first-doc start-doc))
 	      (setf query-time (format nil "~,3F" (/ (- (get-internal-real-time) start-time)
-						    internal-time-units-per-second))))
+						     internal-time-units-per-second))))
 	    (setf num-results (format nil "~A" (length results)))
 	    (setf results (reverse results)))))
-      (format T "~&Results: ~S" results)
+      (format T "~&Results: ~S, start-doc: ~S" results start-doc)
       (with-output-to-string (s)
 	(html-template:fill-and-print-template
 	 main-tmpl
 	 `(:query-string ,escaped-query-string
 	   :query-object ,query-object
-	   :query-parse-tree ,query-parse-tree
 	   :num-results ,num-results
+	   :result-span ,(format nil "Displaying results ~S to ~S." (+ start-doc 1) (+ start-doc (length results)))
+	   :start-doc ,(format nil "~A" (+ start-doc 10))
 	   :query-time ,query-time
-	   :index-size ,(format nil "~A" (montezuma::size *pl-index*))
+	   :index-size ,(format nil "~A" (montezuma::size *index*))
 	   :results ,results)
 	 :stream s)))))
-
