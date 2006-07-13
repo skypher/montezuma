539eb4e45ca08440dd59c059f2fc60db604ca4db
John Wiseman <jjwiseman@gmail.com>
2006-07-13 22:37:53 +0000

    Fixed tar options.

--- montezuma/make-release.sh
+++ montezuma/make-release.sh
@@ -4,4 +4,4 @@ rm -rf docs lucene-in-action clean*
 rm -rf `find . -name .svn`
 cd ..
 mv montezuma montezuma-0.1
-tar cjzf montezuma-0.1.tar.gz montezuma-0.1
+tar czvf montezuma-0.1.tar.gz montezuma-0.1
