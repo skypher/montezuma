636ec603a7f237ab61efb8bfbeb6bbb99cc15783
John Wiseman <jjwiseman@gmail.com>
2006-07-13 22:36:44 +0000

    Just use gzip.

--- montezuma/make-release.sh
+++ montezuma/make-release.sh
@@ -4,4 +4,4 @@ rm -rf docs lucene-in-action clean*
 rm -rf `find . -name .svn`
 cd ..
 mv montezuma montezuma-0.1
-tar cjvf montezuma-0.1.tar.bz2 montezuma-0.1
+tar cjzf montezuma-0.1.tar.bz2 montezuma-0.1
