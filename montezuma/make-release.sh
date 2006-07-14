18ea13e5d2ba09f4dda34329d871a5a5ab73ea6d
John Wiseman <jjwiseman@gmail.com>
2006-07-14 01:18:29 +0000

    We're on 0.1.1 now.

--- montezuma/make-release.sh
+++ montezuma/make-release.sh
@@ -4,5 +4,5 @@ rm -rf docs lucene-in-action clean*
 rm -rf `find . -name .svn`
 rm make-release.sh
 cd ..
-mv montezuma montezuma-0.1
-tar czvf montezuma-0.1.tar.gz montezuma-0.1
+mv montezuma montezuma-0.1.1
+tar czvf montezuma-0.1.1.tar.gz montezuma-0.1.1
