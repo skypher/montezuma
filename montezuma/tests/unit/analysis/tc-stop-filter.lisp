40cedd901729ef5e1b75dd881987def8390c02d6
Leslie P. Polzer <polzer@gnu.org>
2009-02-18 21:22:22 +0000

    Make word file path ASD-relative in tc-stop-filter test file

--- montezuma/tests/unit/analysis/tc-stop-filter.lisp
+++ montezuma/tests/unit/analysis/tc-stop-filter.lisp
@@ -1,14 +1,23 @@
 (in-package #:montezuma)
 
+(defun asdf-system-directory (asdf-system-name)
+  "Computes the directory in which the .asdf file for a given ASDF
+  system resides."
+  (make-pathname :directory
+                 (pathname-directory (truename (asdf:system-definition-pathname
+                                                 (asdf:find-system asdf-system-name))))))
+
+(defun montezuma.asd-relative (path)
+  (merge-pathnames path (asdf-system-directory :montezuma)))
 
 ;; This seems kind of ridiculous.  See
 ;; <http://meme.b9.com/cview.html?channel=lisp&utime=3344192236#utime_requested>;
 ;; even Lisp implementors find it surprising.
 
-(defparameter *word-file-path* (make-pathname :name "wordfile" :type nil
-					      :defaults
-					      (merge-pathnames (make-pathname :directory '(:relative "data"))
-							       *load-pathname*)))
+(defparameter *word-file-path* (montezuma.asd-relative
+                                 (make-pathname :name "wordfile" :type nil
+                                                :defaults
+                                                (make-pathname :directory '(:relative "tests/unit/analysis/data")))))
 
 
 (deftestfun test-stop-filter
