2615f08497a957d81ec5b20a2cb1653bca9815be
Leslie P. Polzer <polzer@gnu.org>
2009-02-19 11:08:05 +0000

    Tweaked make-release.sh

--- montezuma/make-release.sh
+++ montezuma/make-release.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
+VERSION=0.1.3a
+NAME="montezuma-$VERSION"
+
 rm -rf docs lucene-in-action clean*
-rm -rf `find . -name .svn`
+rm -rf `find . -name .svn -o -name \*.fasl`
 rm make-release.sh
 cd ..
-mv montezuma montezuma-0.1.1
-tar czvf montezuma-0.1.1.tar.gz montezuma-0.1.1
+mv montezuma "$NAME"
+tar czvf "$NAME.tar.gz" $NAME
