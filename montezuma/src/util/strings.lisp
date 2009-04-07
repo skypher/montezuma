60631dd66e2e3f598b7b47fdfa5fa670fda6ad2b
Leslie P. Polzer <polzer@gnu.org>
2009-04-07 20:32:37 +0000

    Fixed START/END bug in STRING-TO-BYTES.
    See http://groups.google.com/group/montezuma-dev/browse_thread/thread/17fd29d862d22a61

--- montezuma/src/util/strings.lisp
+++ montezuma/src/util/strings.lisp
@@ -1,17 +1,20 @@
 (in-package #:montezuma)
 
 (defun bytes-to-string (sequence &key (start 0) (end (length sequence)))
-  "Converts a sequence of bytes (unsigned-byte 8) to a string using
-   the implementation's default character encoding."
+  "Converts a sequence of octets to a string using the implementation's
+default character encoding."
    (let ((s (coerce (subseq sequence start end) '(vector (unsigned-byte 8)))))
      (babel:octets-to-string s)))
 
 
-(defun string-to-bytes (string &key (start 0) (end (length string)))
-  "Converts a string to a sequence of bytes (unsigned-byte 8) using
-   the implementation's default character encoding."
-   (let ((s (subseq string start end)))
-     (babel:string-to-octets s)))
+(defun string-to-bytes (string &key (start 0) end)
+  "Converts a string to a sequence of octets using the implementation's
+default character encoding.
+
+NB: START and END denote bounds for the final octet vector,
+not for STRING."
+   (let ((s (babel:string-to-octets s)))
+     (subseq s start (or end (length s)))))
 
 
 (defun string-compare (s1 s2  &key (start1 0) (end1 (length s1)) (start2 0) (end2 (length s2)))
